a6f7cb2a6c1ff985dfeb86b7e4021860caf8b373
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-07 21:44:55 +0200

    agregando emoji al titulo html

--- irina.js
+++ irina.js
@@ -1,4 +1,4 @@
-const mensajes = ["Hola, buenos dias", "No molestes", "No deseas nada, verdad"];
+const mensajes = ["Si tienes dudas, pregunta al papa Google", "No molestes", "El tío Youtube, te puede ayudar"];
 function generate(){
  const rand_first = Math.floor(Math.random()* mensajes.length);
 document.getElementById('result').innerHTML = mensajes[rand_first];
