809695239068ded8872411e98805be2f59e3a5cb
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-07 23:12:37 +0200

    agregando navbar

--- irina.js
+++ irina.js
@@ -1,4 +1,4 @@
-const mensajes = ["Si tienes dudas, pregunta al papa Google", "No molestes", "El tío Youtube, te puede ayudar"];
+const mensajes = ["Si tienes dudas, pregunta al papa Google.", "¡No molestes, gracias!", "El tío Youtube, te puede ayudar."];
 function generate(){
  const rand_first = Math.floor(Math.random()* mensajes.length);
 document.getElementById('result').innerHTML = mensajes[rand_first];
